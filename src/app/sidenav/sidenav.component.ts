32957812bb0a9e8e6d033a53c1e7a93fb4738ada
Sumanth Mahabaleshwar Bhat <sumanth.bhat@arcelormittal.com>
2024-10-02 08:48:00 -0400

    Testing some new icon changes

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { NgClass, NgFor, NgIf } from '@angular/common';
+import { JsonPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, ViewChild, inject, signal, viewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,6 +16,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export interface Tile {
   title: string;
@@ -28,11 +30,20 @@ export interface Tile {
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule, NgIf, MatSlideToggleModule, NgClass, MatCardModule, ScrollingModule, MatGridListModule, NgFor, MatMenuModule, MatFormFieldModule, MatInputModule, MatExpansionModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule, NgIf, MatSlideToggleModule, NgClass, MatCardModule, ScrollingModule, MatGridListModule, NgFor, MatMenuModule, MatFormFieldModule, MatInputModule, MatExpansionModule, MatDatepickerModule,
+    MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
+
+  readonly range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+  
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
   isMobile: boolean = true;
@@ -86,5 +97,18 @@ export class SidenavComponent {
     } else if (this.CustomThemeService.themeSignal() === 'light-mode') {
       this.setLightModeToDisabled = true;
     }
+  }
+
+  selectedDate: Date | null = null;
+  selectedTime: string | null = null;
+
+  // Function to handle date change
+  onDateChange(event: any): void {
+    this.selectedDate = event.value;
+  }
+
+  // Function to handle time change
+  onTimeChange(event: any): void {
+    this.selectedTime = event.target.value;
   }
 }
